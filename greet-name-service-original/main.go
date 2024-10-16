03285cfcb13054d8441e4f8ea42f8b153101c61c
Chirantha Kurukulasuriya <chiranthajk@gmail.com>
2024-10-16 11:22:13 +0530

    fix env names

--- greet-name-service-original/main.go
+++ greet-name-service-original/main.go
@@ -53,10 +53,10 @@ func main() {
 func greetHandler(w http.ResponseWriter, r *http.Request) {
 
 	// OAuth2 Client credentials
-	serviceURL := os.Getenv("CHOREO_MYSERVICE_SERVICE_URL")
-	clientID := os.Getenv("CHOREO_MYSERVICE_CONSUMER_KEY")
-	clientSecret := os.Getenv("CHOREO_MYSERVICE_CONSUMER_SECRET")
-	tokenURL := os.Getenv("CHOREO_MYSERVICE_TOKEN_URL")
+	serviceURL := os.Getenv("CHOREO_CONNECTM1TOT1_SERVICE_URL")
+	clientID := os.Getenv("CHOREO_CONNECTM1TOT1_CONSUMER_KEY")
+	clientSecret := os.Getenv("CHOREO_CONNECTM1TOT1_CONSUMER_SECRET")
+	tokenURL := os.Getenv("CHOREO_CONNECTM1TOT1_TOKEN_URL")
 
 	if serviceURL == "" || clientID == "" || clientSecret == "" || tokenURL == "" {
 		http.Error(w, "Missing required environment variables", http.StatusInternalServerError)
