1fb9a1dfff86bce1684e4577f2a91baa12346236
Chirantha Kurukulasuriya <chiranthajk@gmail.com>
2024-10-31 09:52:55 +0530

    test service to service v11

--- greet-name-service/main.go
+++ greet-name-service/main.go
@@ -12,7 +12,7 @@ import (
 	"syscall"
 	"time"
 
-	"golang.org/x/oauth2/clientcredentials"
+	// "golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
@@ -67,16 +67,17 @@ func worldHandler(w http.ResponseWriter, r *http.Request) {
 // makeOAuth2Request makes an OAuth2 authenticated request to a service
 // Takes in a `serviceType` parameter to determine which environment variables to use
 func makeOAuth2Request(w http.ResponseWriter, r *http.Request, serviceType string) {
-	var serviceURL, clientID, clientSecret, tokenURL string
+	// var serviceURL, clientID, clientSecret, tokenURL string
+	var serviceURL string
 	var testKey, secretKey string
 
 	// Choose environment variables based on the serviceType
 	switch serviceType {
 	case "SERVICE1":
-		serviceURL = os.Getenv("CHOREO_CONNECT_MYSERVICE_TO_TESTSERVICE_SERVICEURL")
-		clientID = os.Getenv("CHOREO_CONNECT_MYSERVICE_TO_TESTSERVICE_CONSUMERKEY")
-		clientSecret = os.Getenv("CHOREO_CONNECT_MYSERVICE_TO_TESTSERVICE_CONSUMERSECRET")
-		tokenURL = os.Getenv("CHOREO_CONNECT_MYSERVICE_TO_TESTSERVICE_TOKENURL")
+		serviceURL = os.Getenv("CHOREO_CONNECT_TO_GREETER_PROJECTACCESS_SERVICEURL")
+		// clientID = os.Getenv("CHOREO_CONNECT_MYSERVICE_TO_TESTSERVICE_CONSUMERKEY")
+		// clientSecret = os.Getenv("CHOREO_CONNECT_MYSERVICE_TO_TESTSERVICE_CONSUMERSECRET")
+		// tokenURL = os.Getenv("CHOREO_CONNECT_MYSERVICE_TO_TESTSERVICE_TOKENURL")
 	case "SERVICE2":
 		serviceURL = os.Getenv("CHOREO_CONNECTIONTOMY3PS_SERVICEURL")
 		testKey = os.Getenv("CHOREO_CONNECTIONTOMY3PS_TESTKEY")
@@ -98,24 +99,28 @@ func makeOAuth2Request(w http.ResponseWriter, r *http.Request, serviceType strin
 	}
 
 	// If SERVICE1, continue with OAuth2 authenticated request
-	if serviceURL == "" || clientID == "" || clientSecret == "" || tokenURL == "" {
+	// if serviceURL == "" || clientID == "" || clientSecret == "" || tokenURL == "" {
+	// 	http.Error(w, "Missing required environment variables", http.StatusInternalServerError)
+	// 	return
+	// }
+	if serviceURL == "" {
 		http.Error(w, "Missing required environment variables", http.StatusInternalServerError)
 		return
 	}
 
-	// Set up OAuth2 configuration
-	oauth2Config := clientcredentials.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		TokenURL:     tokenURL,
-	}
+	// // Set up OAuth2 configuration
+	// oauth2Config := clientcredentials.Config{
+	// 	ClientID:     clientID,
+	// 	ClientSecret: clientSecret,
+	// 	TokenURL:     tokenURL,
+	// }
 
-	// Create an HTTP client with OAuth2 token
-	client := oauth2Config.Client(context.Background())
+	// // Create an HTTP client with OAuth2 token
+	// client := oauth2Config.Client(context.Background())
 	serviceRequestURL := fmt.Sprintf("%s/greeter/greet?name=%s", serviceURL, "person")
 
 	// Make a request to the specified service API path
-	resp, err := client.Get(serviceRequestURL)
+	resp, err := http.Get(serviceRequestURL)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Failed to make a request to service: %v", err), http.StatusInternalServerError)
 		return
